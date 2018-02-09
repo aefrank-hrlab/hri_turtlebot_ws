1d83a1ec5811959346a070b06c0da0263de8944b
aefrank-hrlab <35280604+aefrank-hrlab@users.noreply.github.com>
2018-02-08 16:25:32 -0800

    Update hw6_andi.cpp

--- src/hw6/src/hw6_andi.cpp
+++ src/hw6/src/hw6_andi.cpp
@@ -22,40 +22,46 @@
 #include <time.h>
 #include <math.h>
 
+//		MACROS
+
 // Center of the screen.
 #define SCREEN_CENTER 320
 #define CENTER_THRESHOLD 10
 
-#define GOAL_Z 0.5 // distance at which we consider the target acquired (units unidentified)
+#define GOAL_Z 0.2 		// distance at which we consider the target acquired (units unidentified)
+#define OBS_Z 0.55 		// closet distance the bot will get to an obstacle 
+#define ZTHRESH 0.7		// distance at which points are considered a threat
 
-// Target color detection.
-#define GOAL_R 0
-#define GOAL_G 150
-#define GOAL_B 255
-#define COLOR_THRESHOLD 30
+// DetectedObject color detection.
+#define GOAL_R 120
+#define GOAL_G 199
+#define GOAL_B 147
+#define COLOR_THRESHOLD 0
 
 // PID controller variables.
-#define KP 0.005
+#define KP_Z 0.001
+#define KP 0.001
 #define KD 0.0000001
 #define KI 0.00000001
 #define IMAX 7200000
 #define IMIN 0
-#define ERROR_THRESHOLD 30
-
+#define ERROR_THRESHOLD 0 //0.2
+#define MAX_ERROR 100
 double last_error = 0;
 double i_error = 0;
 
 // PointCloud type definition
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-// Target data
-struct Target {
+// DetectedObject data
+struct DetectedObject {
 	double x;
 	double z;	
 };
-Target target;
+DetectedObject target;
+DetectedObject obstacle;
 
-// Velocity publisher.
+// Velocity publisher
 ros::Publisher vel_pub;
 
 // not entirely sure what this is for?
@@ -67,25 +73,52 @@ bool got_goal_blobs = false;
 /*************************************************************/
 
 // States.
-enum Target_State{
+enum State {
 	NO_TARGET,
 	TARGET_UNCENTERED,
 	TARGET_CENTERED,
-	TARGET_ACQUIRED
+	TARGET_ACQUIRED,
+	PROXIMATE_OBSTACLE
 };
-Target_State state = NO_TARGET;
+State state = NO_TARGET;
 
 void update_state(){
 	// Update state.
-	 if ( abs(target.x - SCREEN_CENTER) < CENTER_THRESHOLD) 			// If within threshold of center...
+	 if ( obstacle.z < OBS_Z ) 			{	state = PROXIMATE_OBSTACLE; }  // If an obstacle is too close, avoid.
+	 else 
+	 if ( abs(target.x - SCREEN_CENTER) < CENTER_THRESHOLD) 		// Otherwise, if within threshold of center...
 	 	if ( target.z <= GOAL_Z ) 	{ 	state = TARGET_ACQUIRED; 	}		// AND close enough, target acquired!
 	 	else 						{ 	state = TARGET_CENTERED; 	}		// BUT not close enough, target centered.
 	 else 							{	state = TARGET_UNCENTERED;	}	// Otherwise, target not centered.
 }
 
-// ACTIONS
+/////////////////////////////////////////////////////////////
+// 						ACTIONS
+
+void avoid(){
+	ROS_INFO("Avoiding...");
+	// turn away from object for 2 seconds, and move forward for 5 seconds
+	geometry_msgs::Twist cmd;
+	cmd.angular.z = obstacle.x - SCREEN_CENTER;
+	vel_pub.publish(cmd);
+	ros::Duration(2.0).sleep();
+
+	// move forward
+	cmd.angular.z = 0;
+	cmd.linear.x = 0.5;
+	vel_pub.publish(cmd);
+	ros::Duration(5.0).sleep();
+
+	update_state();
+}
+
 void wander(){
 	ROS_INFO("Wandering...");
+
+	geometry_msgs::Twist cmd;
+    cmd.linear.x = 0.4;
+    cmd.angular.z = 0.1;
+    vel_pub.publish(cmd);
 }
 
 
@@ -100,12 +133,15 @@ void wander(){
  * 					calculate a control signal to center the 
  * 					x coordinate of the target.
  ***********************************************************/
-double aim() {
+void aim() {
 	ROS_INFO("Aiming...");
+
 	// Calculate PID terms.
 	double error = target.x - SCREEN_CENTER;
+	if(isnan(error)) { error = MAX_ERROR; }
 	double p_term = KP * error;
 	double d_term = KD * (error - last_error);
+
 	// Deal with integral windup.
 	if 		( i_error > IMAX )		{	i_error = IMAX;	}
 	else if ( i_error < IMIN )		{	i_error = IMIN;	}
@@ -116,9 +152,12 @@ double aim() {
 	// Calculate control signal.
 	double ctrl_signal = p_term + d_term + i_term;
 
-	// Only return control signal if error is above threshold.
-	if ( abs(ctrl_signal) < ERROR_THRESHOLD )		{ return 0.0; }
-	else											{ return ctrl_signal;	}
+	// Only publish control signal if error is above threshold.
+	geometry_msgs::Twist cmd;
+    cmd.linear.x = KP_Z * (target.z - GOAL_Z);
+	if ( abs(ctrl_signal) < ERROR_THRESHOLD ) {		cmd.angular.z = 0.0;			}
+	else 									  {		cmd.angular.z = ctrl_signal;	}
+	vel_pub.publish(cmd);
 }
 
 
@@ -136,7 +175,7 @@ double aim() {
 void approach() {
 	ROS_INFO("Approaching target...");
 	geometry_msgs::TwistPtr cmd(new geometry_msgs::Twist());
-    cmd->linear.x = (target.z - GOAL_Z) * KP;
+    cmd->linear.x = (target.z - GOAL_Z) * KP_Z;
     vel_pub.publish(cmd);
 }
 
@@ -174,43 +213,28 @@ void halt() {
  * 					x coordinate of the centroid of the target
  * 					color. State variable is updated to
  *					TARGET_CENTERED or TARGET_UNCENTERED if
- *					needed. This processing is skipped if
- *					state == TARGET_ACQUIRED.
+ *					needed.
  ***********************************************************/
+
 void blob_callback(const cmvision::Blobs& blobs_in) {
-	ROS_INFO("blob_callback");
-	// @TODO: Change this so only blob_callback or cloud_callback processes blobs at once
-	//if (!got_goal_blobs){ 	// skip processing if target has already been acquired
-		double target_sum_x = 0;
-		int i;
-		for (i = 0; i < blobs_in.blob_count; i++) {
-			// if the color is within 10 units of the goal color
-			if (	abs( blobs_in.blobs[i].red 		-	GOAL_R ) 	< COLOR_THRESHOLD &&
-					abs( blobs_in.blobs[i].green	-	GOAL_G )	< COLOR_THRESHOLD &&
-					abs( blobs_in.blobs[i].blue		-	GOAL_B )	< COLOR_THRESHOLD )
-			{
-				 target_sum_x += blobs_in.blobs[i].x; //stores the sum coordinates of the target blobs
-			}
+	double target_sum_x = 0;
+	int i;
+	for (i = 0; i < blobs_in.blob_count; i++) {
+		// if the color is within threshold units of the goal color
+		if (	abs( blobs_in.blobs[i].red 		-	GOAL_R ) 	< COLOR_THRESHOLD &&
+				abs( blobs_in.blobs[i].green	-	GOAL_G )	< COLOR_THRESHOLD &&
+				abs( blobs_in.blobs[i].blue		-	GOAL_B )	< COLOR_THRESHOLD )
+		{
+			 target_sum_x += blobs_in.blobs[i].x; //stores the sum coordinates of the target blobs
 		}
-		// Find average horizontal position of target color centroid and update target.x.
-		 target.x = target_sum_x/i;
+	}
+	// Find average horizontal position of target color centroid and update target.x.
+	 target.x = target_sum_x/i;
 
-		 // Update state.
-		 update_state();
+	 // Update state.
+	 update_state();
 
-	//}
 }
-
-// @TODO: Examine cloud_callback() and make it your own.
-//Threshhold for z distance
-#define ZTHRESH 0.7
-
-uint16_t goal_loc_x = 0; 	//global to store the x coordinate of the goal.
-uint16_t goal_loc_y = 0; 	//global to store the y coordinate of the goal.
-double goal_depth = 0; 		//global storing the depth of the goal blob
-double z_min = 100;
-std::vector<double> PCL_closest_points_z;
-std::vector<double> PCL_closest_points_x;
 
 /******************************************************************
  * Function Name: 	cloud_callback
@@ -225,39 +249,27 @@ std::vector<double> PCL_closest_points_x;
  ****************************************************************/
 void cloud_callback(const PointCloud::ConstPtr& cloud_msg)
 {
-	ROS_INFO("PointCloud callback.");
- //  // 					FILTER DATA
-	// // Container for original & filtered data.
-	// pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-	// pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-	// pcl::PCLPointCloud2 cloud_filtered_pcl;
+	//ROS_INFO("PointCloud callback.");
 
-	// // Convert to PCL data type.
-	// pcl_conversions::toPCL(*cloud_msg, *cloud);
+	// Reset variables.
+	obstacle.z = 1e6;
+	obstacle.x = 0;
+	double count = 0;
 
-	// // Perform the actual filtering.
-	// pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-	// sor.setInputCloud(cloudPtr);
-	// sor.setLeafSize (0.1, 0.1, 0.1);
-	// sor.filter(cloud_filtered_pcl);
-
-	// // Convert to ROS data type.
-	// sensor_msgs::PointCloud2 cloud_filtered;
-	// pcl_conversions::fromPCL(cloud_filtered_pcl, cloud_filtered);
-
- //  // 				FIND CENTROID LOCATION
-	// // Iterate through points.
-	// for ( int j = 0; j < 240; j++ ){
-	// 	for ( int i = 0; i < 640; i++ ) {
-	// 		const pcl::PointXYZ& pt = cloud_filtered->points[ 640 * ( 180 + j ) + i];
-	// 		// Only pay attention to points close enough to bot.
-	// 		if( cloud_filtered.z < ZTHRESH){
-
-	// 		}
-	// 	} 
-	// }
-	// cout << "z: " << output.z << endl;
-
+	// Iterate through points.
+	for ( int j = 0; j < 240; j++ ){
+		for ( int i = 0; i < 640; i++ ) {
+			const pcl::PointXYZ& pt = cloud_msg->points[ 640 * ( 180 + j ) + i];
+			// Only pay attention to points close enough to bot.
+			if( pt.z < ZTHRESH){
+				obstacle.x += obstacle.x;
+				// Of these, find closest distance to obstacle.
+				if ( pt.z < obstacle.z ) { obstacle.z = pt.z; }
+			}
+		} 
+	}
+	obstacle.x /= count;
+	update_state();
 }
 
 
@@ -269,9 +281,6 @@ int main (int argc, char *argv[]){
 	// Initiate ROS processes.
 	ros::init(argc, argv, "hw6");
 	ros::NodeHandle nh;
-
-		// Local variables.
-	geometry_msgs::Twist twist;
 
 	// Create subscriber and publisher for relevant topics.
     ros::Subscriber pc_sub = nh.subscribe<PointCloud>("/camera/depth/points", 1, cloud_callback);  
@@ -286,13 +295,12 @@ int main (int argc, char *argv[]){
 
 	while(ros::ok()){
 		// control flow
-		if 		( state == TARGET_ACQUIRED ) 	{ 	halt();		}
+		if 		( state == PROXIMATE_OBSTACLE ) { 	avoid();	}
+		else if ( state == TARGET_ACQUIRED ) 	{ 	halt();		}
 		else if ( state == TARGET_CENTERED ) 	{ 	approach(); }
 		else if ( state == TARGET_UNCENTERED ) 	{ 	aim();		}
 		else 									{	wander();	}
 
-		// Publish twist message.
-		//vel_pub.publish(twist);
 		// Spin.
 		ros::spinOnce();
 		// Sleep to sample rate.
